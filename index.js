fa6fc60f996ed8d282bfe59b882e0e5c7bd6ecf0
fakanbiomowunmi@gmail.com <fakanbiomowunmi@gmail.com>
2022-10-23 11:26:10 -0400

    Done

--- index.js
+++ index.js
@@ -10,7 +10,32 @@ class Tree {
     this.root = node;
   }
 
-  findNode(data) {}
+  findNode(data) {
+    if(!this.root) return null;
+        if (data === this.root.data) return this.root;
+        
+        let currentNode = this.root;
+        let queue = [ currentNode ];
+
+        while ( queue.length > 0) {
+          // console.log(queue)
+            currentNode = queue.shift();
+           
+  
+            if (currentNode.data === data) {
+              return currentNode 
+            } 
+             if(currentNode.children.length > 0) {
+               for (let i = 0; i < currentNode.children.length; i++) {
+                queue.push(currentNode.children[i])
+               }
+             }
+            
+         
+        }
+        return null
+    }
+
 }
 
 module.exports = { TreeNode, Tree };
